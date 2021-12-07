b2caa830ab6e37d93b494629917ff785a68f6741
Taratouille <tara04.chermak@gmail.com>
2021-12-08 00:09:20 +0100

    add event form

--- frontend/src/components/events.js
+++ frontend/src/components/events.js
@@ -79,7 +79,7 @@ export default function EventListTable(){
         <PopUp />
         <Table className='tableInside' columns={columns} data={dat}/>
         <div className="addEvent">
-          <button className="addBut" onClick={() => handleAdd()}> + </button>
+          <button className="addBut" id="addBut" onClick={() => handleAdd()}> + </button>
           <AddEvent />
           <div class="fix"></div>
         </div>
@@ -88,14 +88,17 @@ export default function EventListTable(){
 }
 
 function handleAdd(){
-  console.log("yo");
   let add = document.getElementById("add");
     if(add.style.display!="flex"){
       add.style.display = "flex";
+      document.getElementById("addBut").innerHTML='-';
       console.log("on affiche");
     }
-    else
-    add.style.display = "none";
+    else{
+      add.style.display = "none";
+      document.getElementById("addBut").innerHTML='+';
+    }
+    
 }
 
 function ClickLink(params){
@@ -114,10 +117,79 @@ function PopUp(params, dvalue){
   );
 }
 
+/*
+<div className="date">
+          <select>
+            <option value={year.getFullYear()}>{year.getFullYear()}</option>
+            <option value={year.getFullYear() + 1}>{year.getFullYear()+1}</option>
+            <option value={year.getFullYear() + 2}>{year.getFullYear()+2}</option>
+            <option value={year.getFullYear() + 3}>{year.getFullYear()+3}</option>
+          </select>
+          <select>
+            <option value="1">Jan</option>
+            <option value='2'>Fev</option>
+            <option value='3'>Apr</option>
+            <option value='4'>Mar</option>
+            <option value='5'>May</option>
+            <option value='6'>Jun</option>
+            <option value='7'>Jul</option>
+            <option value='8'>Aug</option>
+            <option value='9'>Sep</option>
+            <option value='10'>Oct</option>
+            <option value='11'>Nov</option>
+            <option value='12'>Dec</option>
+          </select>
+          <select>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+            <option value='6'>6</option>
+            <option value='7'>7</option>
+            <option value='8'>8</option>
+            <option value='9'>9</option>
+            <option value='10'>10</option>
+            <option value='11'>11</option>
+            <option value='12'>12</option>
+            <option value='13'>13</option>
+            <option value='14'>14</option>
+            <option value='14'>15</option>
+            <option value='16'>16</option>
+            <option value='17'>17</option>
+            <option value='18'>18</option>
+            <option value='19'>19</option>
+            <option value='20'>20</option>
+            <option value='21'>21</option>
+            <option value='22'>22</option>
+            <option value='23'>23</option>
+            <option value='24'>24</option>
+            <option value='25'>25</option>
+            <option value='26'>26</option>
+            <option value='27'>27</option>
+            <option value='28'>28</option>
+            <option value='29'>29</option>
+            <option value='30'>30</option>
+            <option value='31'>31</option>
+          </select>
+          </div>
+*/
+
 function AddEvent(){
+  let year=new Date();
   return (
     <div id="add">
-      Ecris des trucs
+      <h3>Add an Event</h3>
+      <form >
+        <input type="text" placeholder="Event Name" />
+        <input type="text" placeholder="Event Date" />
+        <div className="date">
+          <label>Date of the event</label>
+          <input type="date"/>
+        </div>
+        <input type="text" placeholder="Couvreurs" />
+        <input type="submit"/>
+      </form>
     </div>
   )
 }
