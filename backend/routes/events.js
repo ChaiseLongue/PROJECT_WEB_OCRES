4c5e1ae22a0583caee8be09df705e9a7a1755355
ChaiseLongue <57494281+ChaiseLongue@users.noreply.github.com>
2021-12-08 01:31:20 +0100

    all routes CRUD

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 // ----------------------------- MongoDB ----------------------------------
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const url = "mongodb+srv://admin:admin@cluster0.2ztwl.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 // ------------------------------------------------------------------------
@@ -48,26 +48,111 @@ router.get('/all', async function(req, res, next) {
     res.status(200).send(response);
 });
 
-/* UPDATE user. 
+// UPDATE event by id
+//To delete send
+//   /id?json=JSON.stringify(json object)
 router.post('/:id', async function(req, res) {
 
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
-    console.log(req.params.id);
+    console.log(req.query.id);
 
     await client.connect();
     console.log("Connected correctly to server");
     const db = client.db("jbtv");
     const col = db.collection("eventList");
-    const result = await col.updateOne({_id: req.params.id}, {$set: {"event" : "yo"}});
+    const result = await col.updateOne({_id: ObjectId(req.params.id)}, {$set: JSON.parse(req.query.json)});
     console.log(result);
 
     // Return message
     res.json({
-      message: `Just updated ${req.params.id} with yo`
+      message: `Just updated ${req.query.id} with ${req.query.json}`
   });
-});*/
-  
+});
+
+// UPDATE event by any field !!! ATTENTION TO REDONDENCIES
+//To update send
+//   /JSON.stringify(json object)?to=JSON.stringify(json object)
+router.post('/any/:from', async function(req, res) {
+
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db("jbtv");
+    const col = db.collection("eventList");
+
+    //const tst = {event : "NEW"};
+    //console.log(tst);
+    //console.log(JSON.stringify(tst));
+    //console.log(req.query.json);
+    //console.log(JSON.parse(req.query.json));
+    
+    const result = await col.updateOne(JSON.parse(req.params.from), {$set: JSON.parse(req.query.to)});
+    console.log(result);
+
+    // Return message
+    res.json({
+      message: `Just updated with ${req.query.to}`
+  });
+});
+
+// ADD event 
+//To add send
+//   /?event=...&date=...(ISOFormat date)&name=... etc
+router.put('/', async function(req, res) {
+
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+    console.log(req.query.id);
+
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db("jbtv");
+    const col = db.collection("eventList");
+    const result = await col.insertOne(
+        {
+            event: req.query.event,
+            date: new Date(req.query.date),
+            position:
+            {
+                name: req.query.name,
+                coord: 
+                {
+                    lat: req.query.lat,
+                    lng: req.query.lng
+                }    
+            },
+            couvreurs: req.query.couvreurs
+        });
+    console.log(result);
+
+    // Return message
+    res.json({
+      message: `Just added ${req.query.name}`
+  });
+});
+
+//To delete send
+//   /?json=JSON.stringify(json object)
+router.delete('/', async function(req, res) {
+
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+    console.log(req.query.id);
+
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db("jbtv");
+    const col = db.collection("eventList");
+    const result = await col.deleteOne(JSON.parse(req.query.json));
+    console.log(result);
+
+    // Return message
+    res.json({
+      message: `Just deleted ${req.query.json}`
+  });
+});
 
 /*
 async function run() {
